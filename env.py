31d60d9833fad9a4808d24fbbf1efa7fd9348e44
umidyor asomov <106219812+umidyor@users.noreply.github.com>
2024-12-24 18:15:15 +0500

    change service folder

--- env.py
+++ env.py
@@ -2,7 +2,7 @@ import requests
 import psycopg2
 from time import sleep
 AMOCRM_SUBDOMAIN = 'pixeltechuz'  # Replace with your AmoCRM subdomain
-ACCESS_TOKEN = open("access_token.txt",mode="r",encoding="UTF-8").read().strip()
+ACCESS_TOKEN = open("access_token.txt", mode="r", encoding="UTF-8").read().strip()
 API_URL = f'https://{AMOCRM_SUBDOMAIN}.amocrm.ru/api/v4'
 
 
